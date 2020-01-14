3cdd21531f042913e1e8cf197428de4c83931259
CliffGreene <lifeguardtwenty@gmail.com>
2020-01-14 21:34:49 +0000

    created timer function

---
+++ timer.js
@@ -0,0 +1,9 @@
+let seconds = process.argv.slice(2);
+for(let i = 0; i < seconds.length; i++) {
+  const timer = seconds[i] * 1000;
+    if(timer > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, timer)
+    }
+  }
